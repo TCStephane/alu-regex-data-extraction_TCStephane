455beafe7dd04df18e9d7eb0946f497564669fd4
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-05-24 22:16:11 +0200

    email extraction implemented

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ with open("input/raw-text.txt", "r") as f:
 email_pattern = r'[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,}'
 card_pattern = r'\b\d{4}[\s-]\d{4,6}[\s-]\d{4,6}[\s-]\d{3,4}\b'
 all_mails = re.findall(email_pattern, text)
+all_cards = re.findall(card_pattern, text)
 
 def is_safe_email(email):
     if any(char in email for char in ["'", '"', "<", ">", ";", "--"]):
@@ -29,3 +30,23 @@ general_emails = [e for e in safe_emails if not any(e.endswith(d) for d in alu_d
 
 print("\n\nGeneral Emails: ", general_emails)
 print("\n\nALU Emails: ", alu_emails)
+
+def is_valid_card(card):
+    digits_only = re.sub(r'[\s-]', '', card)
+
+    if not digits_only.isdigit():
+        return False
+
+    if not (15 <= len(digits_only) <= 16):
+        return False
+    return True
+
+safe_cards = [c for c in all_cards if is_valid_card(c)]
+
+def mask_card(card):
+    digits_only = re.sub(r'[\s-]', '', card)
+    return "**** **** ****" + digits_only[-4:]
+
+masked_cards = [mask_card(c) for c in safe_cards]
+
+print("\n\nCredit Cards (masked):", masked_cards)
