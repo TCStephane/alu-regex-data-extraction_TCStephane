f6d9de7827dba302d45c7a3b99e5cce174f703d7
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-05-24 19:35:24 +0200

    creatng safe email validator

--- src/main.py
+++ src/main.py
@@ -5,3 +5,16 @@ with open("input/raw-text.txt", "r") as f:
     text = f.read()
 
 email_pattern = r'[\w\.-]+@[\w\.-]+\.[a-zA-Z]{2,}'
+all_mails = re.findall(email_pattern, text)
+
+def is_safe_email(email):
+    if any(char in email for char in ["'", '"', "<", ">", ";", "--"]):
+        return False
+    
+    if email.count("@") != 1:
+        return False
+    
+    if ".." in email.split("@")[1]:
+        return False
+    
+    return True
